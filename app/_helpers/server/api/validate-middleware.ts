0ab16eab4faf681935ca6e67a23ef1694e9d757c
dabeng <dabeng413@gmail.com>
2025-01-08 10:16:15 +0800

    introduce zod

--- app/_helpers/server/api/validate-middleware.ts
+++ app/_helpers/server/api/validate-middleware.ts
@@ -1,23 +1,34 @@
 import joi from 'joi';
+import { z, ZodError } from "zod";
 
 export { validateMiddleware };
 
-async function validateMiddleware(req: Request, schema: joi.ObjectSchema) {
-    if (!schema) return;
+async function validateMiddleware<T extends z.ZodTypeAny>(req: Request, schema: T) {
+	if (!schema) return;
 
-    const options = {
-        abortEarly: false, // include all errors
-        allowUnknown: true, // ignore unknown props
-        stripUnknown: true // remove unknown props
-    };
+	const options = {
+		abortEarly: false, // include all errors
+		allowUnknown: true, // ignore unknown props
+		stripUnknown: true // remove unknown props
+	};
 
-    const body = await req.json();
-    const { error, value } = schema.validate(body, options);
+	const body = await req.json();
+	const result = schema.safeParse(body);
 
-    if (error) {
-        throw `Validation error: ${error.details.map(x => x.message).join(', ')}`;
-    }
+	if (!result.success) {
+		/* error.issues
+		[
+	        {
+	        	"code": "invalid_type",
+	        	"expected": "string",
+	        	"received": "number",
+	        	"path": [ "name" ],
+	        	"message": "Expected string, received number"
+	        }
+    ] */
+		throw `Validation error: ${result.error.issues.map(x => x.message + ' at "' + x.path + '"').join(', ')}`;
+	}
 
-    // update req.json() to return sanitized req body
-    req.json = () => value;    
+	// update req.json() to return sanitized req body
+	req.json = () => result.data;
 }
--- app/api/account/register/route.ts
+++ app/api/account/register/route.ts
@@ -1,31 +1,31 @@
-import joi from "joi";
+import { z } from "zod";
 
 import { NextRequest, NextResponse } from "next/server";
 import { usersRepo } from "_helpers/server";
 import {
-    errorHandler,
-    jwtMiddleware,
-    validateMiddleware
+	errorHandler,
+	jwtMiddleware,
+	validateMiddleware
 } from "_helpers/server/api";
 
 export async function POST(req: NextRequest) {
-    try {
-        // global middleware
-        await jwtMiddleware(req);
-        const schema = joi.object({
-            firstName: joi.string().required(),
-            lastName: joi.string().required(),
-            username: joi.string().required(),
-            password: joi.string().min(6).required()
-        });
-        await validateMiddleware(req, schema);
+	try {
+		// global middleware
+		await jwtMiddleware(req);
+		const schema = z.object({
+			firstName: z.string(),
+			lastName: z.string(),
+			username: z.string(),
+			password: z.string().min(6)
+		});
+		await validateMiddleware(req, schema);
 
-        // route handler
-        const body = await req.json();
-        await usersRepo.create(body);
-        return NextResponse.json({});
-    } catch (err: any) {
-        // global error handler
-        return errorHandler(err);
-    }
+		// route handler
+		const body = await req.json();
+		await usersRepo.create(body);
+		return NextResponse.json({});
+	} catch (err: any) {
+		// global error handler
+		return errorHandler(err);
+	}
 }
